8111c2a18ab5e11ce06a1fdccb64ff83e6a592ff
Martin Bůna <martin.buna@gmail.com>
2016-11-07 13:43:49 +0100

    Upraveno vyhledavani eventu

--- common/models/event.js
+++ common/models/event.js
@@ -1,5 +1,42 @@
 'use strict';
 
 module.exports = function(Event) {
+  Event.findFulltext = (name, desc, callback) => {
+    Event.find({
+      where: {
+        or: [
+          {
+            "name": new RegExp(name, "i")
+          }, {
+            "desc": new RegExp(desc, "i")
+          }
+        ]
+      }
+    }, (error, events) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, events);
+    })
+  };
+
+  Event.remoteMethod('findFulltext', {
+    accepts: [
+      {
+        arg: 'name',
+        type: 'string'
+      }, {
+        arg: 'desc',
+        type: 'string'
+      }
+    ],
+    returns: {
+      arg: 'events',
+      type: 'string'
+    },
+    http: {
+      verb: 'get'
+    }
+  });
 
 };
