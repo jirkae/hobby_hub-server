e6e88c4bda1622168495a6f3fbde08fe84365670
Martin Bůna <martin.buna@gmail.com>
2016-11-30 11:34:18 +0100

    getCurrentUser metoda

--- common/models/auth-user.js
+++ common/models/auth-user.js
@@ -1,5 +1,35 @@
 'use strict';
+var LoopBackContext = require('loopback-context');
+var app = require('../../server/server');
 
-module.exports = function(Authuser) {
+module.exports = function(AuthUser) {
+  AuthUser.getCurrentUser = (callback) => {
+    //current user
+    var ctx = LoopBackContext.getCurrentContext();
+    var currentUser = ctx && ctx.get('currentUser');
+    var currentUserId = currentUser['id'];
+
+    AuthUser.findOne({
+      where: {
+        id: currentUserId
+      }
+    }, (error, user) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, user);
+    })
+  }
+
+  AuthUser.remoteMethod('getCurrentUser', {
+    accepts: [],
+    returns: {
+      arg: 'user',
+      type: 'array'
+    },
+    http: {
+      verb: 'get'
+    }
+  });
 
 };
